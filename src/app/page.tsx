bf0935d2c2f516e3e921c47ce03776fc503f6a8a
prakharsharma13 <115884239+prakharsharma13@users.noreply.github.com>
2025-01-02 02:43:33 +0530

    Feed component design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,34 @@
-import Image from "@/components/Image";
+import Feed from "@/components/Feed";
+import Share from "@/components/Share";
+import Link from "next/link";
 
 const Homepage = () => {
-  return <div className=""></div>;
+  return (
+    <div className="">
+      <div className=" px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray">
+        <Link
+          href="/"
+          className="pb-3 flex items-center border-b-4 border-iconBlue"
+        >
+          For you
+        </Link>
+        <Link href="/" className="pb-3 flex items-center ">
+          Following
+        </Link>
+        <Link href="/" className="pb-3 flex items-center ">
+          React.js
+        </Link>
+        <Link href="/" className="pb-3 flex items-center ">
+          Javascript
+        </Link>
+        <Link href="/" className="pb-3 flex items-center ">
+          CSS
+        </Link>
+      </div>
+      <Share />
+      <Feed />
+    </div>
+  );
 };
 
 export default Homepage;
